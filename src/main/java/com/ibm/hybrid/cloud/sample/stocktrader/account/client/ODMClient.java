25972fe5125807e1794914e0526c0da0419bfaa4
rtclauss <ryan.claussen@kyndryl.com>
2023-11-07 17:55:44 -0600

    remove autocloseable

--- src/main/java/com/ibm/hybrid/cloud/sample/stocktrader/account/client/ODMClient.java
+++ src/main/java/com/ibm/hybrid/cloud/sample/stocktrader/account/client/ODMClient.java
@@ -18,7 +18,10 @@
 package com.ibm.hybrid.cloud.sample.stocktrader.account.client;
 
 import com.ibm.hybrid.cloud.sample.stocktrader.account.json.ODMLoyaltyRule;
-import jakarta.ws.rs.*;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
 import org.eclipse.microprofile.rest.client.inject.RegisterRestClient;
 
 @Path("/")
